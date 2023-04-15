5d53f72930c1f1c9f5ec0922edc18a8905854ccf
Daft <daftdevs@gmail.com>
2023-04-16 00:06:40 +0700

    feat: init redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// creates a Redux store, and also automatically configure the Redux DevTools extension
+// for inspectting the store while developing
+
+export default configureStore({
+  reducer: {},
+});
