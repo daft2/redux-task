8e7037d460cd8aaa23f1d13c5ae516e7e92ed273
Daft <daftdevs@gmail.com>
2023-04-16 01:02:38 +0700

    ref: add typescript type & taskReducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import taskReducer from "./slices/taskSlice";
 // creates a Redux store, and also automatically configure the Redux DevTools extension
 // for inspectting the store while developing
 
-export default configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    task: taskReducer,
+  },
 });
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {task: Task}
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
