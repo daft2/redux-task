e154e2c92aa7cc11f657f3d23991d3be08098708
Daft <daftdevs@gmail.com>
2023-04-16 20:50:49 +0700

    chore: add and setup MUI component, icons, and typography

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
+    <CssBaseline />
     <App />
   </Provider>
 );
